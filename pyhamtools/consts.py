d12f83c9c23544e87ff4fe325faa6118dac5dd88
dh1tw <tobias@dh1tw.de>
2014-04-24 15:06:28 +0200

    added constants

---
+++ pyhamtools/consts.py
@@ -0,0 +1,20 @@
+
+
+class LookupConventions:
+    """ This class defines the constants used within the pyhamtools package """
+
+    CALLSIGN = "callsign"
+    COUNTRY = "country"
+    PREFIX = "prefix"
+    ADIF = "adif"
+    CQZ = "cqz"
+    ITUZ = "ituz"
+    CONTINENT = "continent"
+    LATITUDE = "latitude"
+    LONGITUDE = "longitude"
+    START = "start"
+    END = "end"
+    WHITELIST = "whitelist"
+    WHITELIST_START = "whitelist_start"
+    WHITELIST_END = "whitelist_end"
+    DELETED = "deleted"
