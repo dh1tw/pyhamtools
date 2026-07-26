8c31e2fdb49ff4d34f336c5be2a25d6579a92f47
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2026-07-27 01:21:36 +0200

    release v0.13.0

--- pyhamtools/version.py
+++ pyhamtools/version.py
@@ -1,3 +1,3 @@
-VERSION = (0, 12, 0)
-__release__ = ''.join(['-.'[type(x) == int]+str(x) for x in VERSION])[1:]
-__version__ = '.'.join((str(VERSION[0]), str(VERSION[1])))
+VERSION = (0, 13, 0)
+__release__ = "".join(["-."[type(x) == int] + str(x) for x in VERSION])[1:]
+__version__ = ".".join((str(VERSION[0]), str(VERSION[1])))
